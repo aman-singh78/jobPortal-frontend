c6d2f2f94d9bfe902a8af943e0f26762c2a6d4dc
Aman Thapa <amansinghthapa33@gmail.com>
2025-07-29 22:10:22 +0530

    Navbar implement

--- src/components/ui/shared/NavBar.jsx
+++ src/components/ui/shared/NavBar.jsx
@@ -1,5 +1,12 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "../button";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 
 const NavBar = () => {
   return (
@@ -11,12 +18,50 @@ const NavBar = () => {
           </h1>
         </div>
 
-        <div>
+        <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
             <li>Home</li>
             <li>Jobs</li>
             <li>Browse </li>
           </ul>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage
+                  className="w-12 rounded-lg"
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+              <div>
+                <div className="flex gap-4 space-y-2">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      className="w-12 rounded-lg"
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                  </Avatar>
+                  <div>
+                    <h3 className="font-medium"> Aman Mernstack</h3>
+                    <p className="text-sm text-muted-foreground">
+                      Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-3 text-gray-600">
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <Button variant="link">View Profile</Button>
+                  </div>
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <Button variant="link">Logout</Button>
+                  </div>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
       </div>
     </div>
