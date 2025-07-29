baa4ea0231929a6a2b9f3edf5c78c419bf18fd9f
Aman Thapa <amansinghthapa33@gmail.com>
2025-07-29 21:19:12 +0530

    JobPortal Frontend

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import NavBar from "./components/ui/shared/NavBar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
