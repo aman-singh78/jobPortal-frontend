64521fda3ce2b4790c012c5a81a711a47757e6c4
Aman Thapa <amansinghthapa33@gmail.com>
2025-08-01 21:07:13 +0530

    Loading implemented in signup

--- src/components/ui/auth/Signup.jsx
+++ src/components/ui/auth/Signup.jsx
@@ -8,6 +8,9 @@ import { Link, useNavigate } from "react-router-dom";
 import { USER_API_END_POINT } from "@/utils/constant";
 import axios from "axios";
 import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import { Loader2 } from "lucide-react";
 
 const Signup = () => {
   const [input, setInput] = useState({
@@ -19,6 +22,8 @@ const Signup = () => {
     file: "",
   });
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { loading } = useSelector((store) => store.auth);
 
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
@@ -40,6 +45,7 @@ const Signup = () => {
     }
     e.preventDefault();
     try {
+      dispatch(setLoading(true));
       const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
         withCredentials: true,
       });
@@ -52,6 +58,8 @@ const Signup = () => {
     } catch (error) {
       console.log(error);
       toast.error(error.response.data.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -139,9 +147,15 @@ const Signup = () => {
               />
             </div>
           </div>
-          <Button type="submit" className="w-full my-4">
-            Signup
-          </Button>
+          {loading ? (
+            <Button className="w-full my-4">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+            </Button>
+          ) : (
+            <Button type="submit" className="w-full my-4">
+              SignUp
+            </Button>
+          )}
           <span className="text-small">
             Already have an account?
             <Link to="/login" className="text-blue-600">
