54984e44093a6486938f8d49f4b45e9bafad75de
Aman Thapa <amansinghthapa33@gmail.com>
2025-08-01 21:50:48 +0530

    installed carousel from shadcn

--- src/components/CategoryCarousel.jsx
+++ src/components/CategoryCarousel.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Button } from "./ui/button";
 
 const category = [
   "Frontend Developer",
@@ -9,7 +17,21 @@ const category = [
 ];
 
 const CategoryCarousel = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Carousel className="w-full max-w-xl mx-auto my-20">
+        <CarouselContent>
+          {category.map((cat, index) => (
+            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+              <Button>{cat}</Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 };
 
 export default CategoryCarousel;
