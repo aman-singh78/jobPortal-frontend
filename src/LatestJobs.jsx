c2e80525d36d8cb8a0379ace990800e402563407
Aman Thapa <amansinghthapa33@gmail.com>
2025-08-01 21:59:09 +0530

    implementing latestJob cards

--- src/LatestJobs.jsx
+++ src/LatestJobs.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import LatestJobCards from "./components/LatestJobCards";
+
+const randomJobs = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const LatestJobs = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-7xl mx-auto my-20">
+      <h1 className="text-4xl font-bold">
+        <span className="text-[#6A38C2]">Latest & Top</span>Job Openings
+      </h1>
+      {randomJobs.map((item, index) => (
+        <LatestJobCards />
+      ))}
+    </div>
+  );
 };
 
 export default LatestJobs;
---
+++ src/components/LatestJobCards.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LatestJobCards = () => {
+  return <div>jwwi1</div>;
+};
+
+export default LatestJobCards;
